b9ba8f7da2a6a6d32af6afa6241c864837711f58
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-07-16 13:54:33 -0400

    Adds solution to anagrams.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,49 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function cleanString(string) {
+  const str = string.toLowerCase().split("").toString();
+  const regex = new RegExp(/[^a-z]/g);
+
+  return str.replace(regex, "");
+}
+
+function anagrams(stringA, stringB) {
+  const cleanA = cleanString(stringA).split("").sort().join("");
+  const cleanB = cleanString(stringB).split("").sort().join("");
+
+  return cleanA === cleanB;
+}
 
 module.exports = anagrams;
+
+// function createHistogram(string) {
+//   const obj = new Object();
+//
+//   for (const character of string) {
+//     if (obj[character]) {
+//       obj[character] += 1;
+//     } else {
+//       obj[character] = 1;
+//     }
+//   }
+//   return obj;
+// }
+//
+// function anagrams(stringA, stringB) {
+//   const cleanA = cleanString(stringA).split("").sort().join("");
+//   const cleanB = cleanString(stringB).split("").sort().join("");
+//
+//   if (cleanA.length !== cleanB.length) {
+//     return false;
+//   }
+//
+//   const histogramA = createHistogram(cleanA);
+//   const histogramB = createHistogram(cleanB);
+//
+//   for (const element in histogramA) {
+//     if (histogramA[element] !== histogramB[element]) {
+//       return false;
+//     }
+//   }
+//   return true;
