37585ad2c9b17ec1480d2e7081eb9631c3209839
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-07-21 18:29:41 -0400

    Adds solution and comments to vowels.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  return str.match(/[aeiou]/gi)?.length || 0;
+}
 
 module.exports = vowels;
+
+// return str.match(/[aeiou]/gi)?.length || 0;
+// is same as below:
+// const regex = /[aeiou]/gi;
+// const vowels = str.match(regex) || [];
+// return vowels.length;
+//wouldn't create memory refs or exec contexts for the variables regex and vowels.
+
+// function vowels(str) {
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   let counter = 0;
+//   for (const letter of str) {
+//     if (vowels.includes(letter.toLowerCase())) {
+//       counter += 1;
+//     }
+//   }
+//   return counter;
+// }
