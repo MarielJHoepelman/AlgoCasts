07be64f9c5be23de7c46de59d219d6dff9c8eb31
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-07-21 17:45:38 -0400

    Refactors and adds comments to pyramid.

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -18,14 +18,33 @@
 //  #####
 // #######
 function pyramid(n) {
-  for (row = 0; row <= n - 1; row++) {
+  for (row = 0; row < n; row++) {
     let matrix = "";
-    for (column = 1; column <= n * 2 - 1; column++) {
-      const left = n - row;
-      const right = n + row;
+    for (column = 0; column < n * 2 - 1; column++) {
+      const left = n - row - 1;
+      const right = n + row - 1;
       matrix += column >= left && column <= right ? "#" : " ";
     }
     console.log(matrix);
   }
 }
+
 module.exports = pyramid;
+
+// Stephen's solution
+// function pyramid(n) {
+//   // const mid = Math.floor((2 * n - 1) / 2);
+// why use Math floor if sol is always n - 1????
+//   const mid = n - 1;
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (mid - row <= column && mid + row >= column) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
