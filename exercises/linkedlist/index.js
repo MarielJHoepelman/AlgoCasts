fa63d59e31776773bff89b68d71f371ddedbeda6
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-08-13 17:23:09 -0400

    WIP

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -52,14 +52,73 @@ class LinkedList {
       return;
     }
     //if head and head next is null clear head
-    if (this.head.next === null) {
-      this.clear();
-    }
-
-    if (this.head && this.head.next) {
+    // if (this.head.next === null) {
+    //   this.clear();
+    // }
+    //
+    if (this.head) {
       this.head = this.head.next;
     }
   }
-}
 
+  removeLast() {
+    //if no head return
+    if (this.head === null) {
+      return;
+    }
+    //if no next node clear
+    if (this.head.next === null) {
+      this.clear();
+      return;
+    }
+
+    let node = this.head;
+    while (node.next) {
+      node.next.next ? (node = node.next) : (node.next = null);
+    }
+  }
+
+  insertLast(data) {
+    const node = new Node(data);
+    this.head ? (this.getLast().next = node) : (this.head = node);
+  }
+
+  getAt(integer) {
+    if (this.head === null) {
+      return null;
+    }
+
+    let counter = 0;
+    let current = this.head;
+    while (counter < integer) {
+      counter++;
+      current = current.next;
+    }
+    return current;
+  }
+
+  removeAt(integer) {
+    if (this.head === null) {
+      return;
+    }
+
+    if (integer === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let current = this.head;
+    let counter = 0;
+
+    while (counter < integer) {
+      if (current.next === null) {
+        this.removeLast();
+      } else if (current.next === this.getAt(integer)) {
+        current.next = this.getAt(integer).next;
+      }
+      current = current.next;
+      counter++;
+    }
+  }
+}
 module.exports = { Node, LinkedList };
